9c051efaad7210706f9acf95e70d0e1b358ce9b8
He Outong <heoutong@gmail.com>
2021-06-10 15:09:25 +0800

    Adding user login welcome functionality

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -72,7 +72,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(f"/{username}")
         flash(error)
 
     return render_template('auth/login.html')
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -17,3 +17,14 @@ def index():
      ).fetchall()
      return render_template('blog/index.html', posts=posts)
 
+
+@bp.route('/<userName>', endpoint='func1')
+def index(userName):
+     db = get_db()
+     posts = db.execute(
+         'SELECT p.id, title, body, created, author_id, username'
+         ' FROM post p JOIN user u ON p.author_id = u.id'
+         ' ORDER BY created DESC'
+     ).fetchall()
+     return render_template('blog/user.html', posts=posts, userName=userName)
+
